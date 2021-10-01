0430be56f5cb59465019013a780674bf72ac9896
Johnny Camacho <joh_camacho@outlook.com>
2021-10-01 17:01:50 -0300

    Initial Commit

---
+++ Mini-Challenge-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini-Challenge-2
+//
+//  Created by Johnny Camacho on 01/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
