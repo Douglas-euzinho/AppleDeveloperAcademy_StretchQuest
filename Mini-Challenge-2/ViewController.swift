5f7db27d16ea69dd48308118819c39e428ad1faa
Douglas Figueirôa <douguerpw2@hotmail.com>
2021-10-04 14:28:57 -0300

    feat: label view controller

--- Mini-Challenge-2/ViewController.swift
+++ Mini-Challenge-2/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var texto1: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
