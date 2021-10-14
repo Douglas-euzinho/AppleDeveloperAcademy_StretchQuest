cb2b7c75971c513a44978e80e5976430b1255cb2
Johnny Camacho <joh_camacho@outlook.com>
2021-10-14 17:34:49 -0300

    Creating screen stretch.

---
+++ Mini-Challenge-2/View/Controller/StretchViewController.swift
@@ -0,0 +1,26 @@
+//
+//  StretchViewController.swift
+//  Mini-Challenge-2
+//
+//  Created by Johnny Camacho on 14/10/21.
+//
+
+import UIKit
+
+class StretchViewController: UIViewController {
+    
+    // MARK: - Properties
+    @IBOutlet weak var animation: UIImageView!
+    @IBOutlet weak var descriptionStretch: UILabel!
+    @IBOutlet weak var timerElipse: UIImageView!
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
