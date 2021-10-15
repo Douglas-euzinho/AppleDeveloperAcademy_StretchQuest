9911dfa67038e73bf0085f2738550b36ff22f7eb
Iorgers Almeida <iorgerss3@gmail.com>
2021-10-15 14:17:19 -0300

    feature: basic StretchViewController implementation for testing purposes

--- Mini-Challenge-2/View/Controller/StretchViewController.swift
+++ Mini-Challenge-2/View/Controller/StretchViewController.swift
@@ -6,21 +6,56 @@
 //
 
 import UIKit
+import Core
 
-class StretchViewController: UIViewController {
-    
+class StretchViewController: UIViewController, OnStretchListener{
+ 
     // MARK: - Properties
     @IBOutlet weak var animation: UIImageView!
     @IBOutlet weak var descriptionStretch: UILabel!
     @IBOutlet weak var timerElipse: UIImageView!
     @IBOutlet weak var timerLabel: UILabel!
     
+    var viewModel: StretchesViewModel!
+    
+    var agendamentos = 0
+    var contador = 0
+    
+    var stretchDuration = 30
+    
+    func onStretchChanged(stretch: Stretch) {
+        descriptionStretch.text = stretch.title
+        self.stretchDuration = Int(stretch.durationInSeconds)
+        
+        if agendamentos < 5 {
+            agendamentos += 1
+            tick()
+        }
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.viewModel.listener = self
+        self.viewModel.startSession(with: .posture)
     }
     
+    func tick(){
+        
+        contador += 1
+        contador = contador % (self.stretchDuration + 1)
+        
+        self.timerLabel.text = "\(contador)"
+        
+        if contador < self.stretchDuration {
+            tock()
+        }else{
+            self.viewModel.nextStretch()
+        }
+    }
     
-
+    func tock(){
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.tick()
+        }
+    }
 }
