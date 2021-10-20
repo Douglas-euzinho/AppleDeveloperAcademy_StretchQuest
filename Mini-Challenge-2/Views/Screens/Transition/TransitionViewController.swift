57f8e9b625cb914efa9f49ff8d5f965cf3ac9eac
Johnny Camacho <joh_camacho@outlook.com>
2021-10-20 02:25:22 -0300

    Creating outlets screen Transition.

--- Mini-Challenge-2/Views/Screens/Transition/TransitionViewController.swift
+++ Mini-Challenge-2/Views/Screens/Transition/TransitionViewController.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 class TransitionViewController: UIViewController {
-
+    
+    @IBOutlet weak var animation: UIView!
+    
+    @IBOutlet weak var timerElipse: UIImageView!
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
