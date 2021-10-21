47d6b8f979f2b288a1341e7254ca9ed166da0ae5
Iorgers Almeida <iorgerss3@gmail.com>
2021-10-20 23:30:28 -0300

    feature: added animation for transition screen

--- Mini-Challenge-2/Views/Screens/Transition/TransitionViewController.swift
+++ Mini-Challenge-2/Views/Screens/Transition/TransitionViewController.swift
@@ -15,17 +15,121 @@ typealias OnDismiss = () -> ()
 
 class TransitionViewController: UIViewController {
 
+    lazy var circleAttributed = UIColor.getColorBy(category: .posture)
+    
+    var pulsatingLayer = CAShapeLayer()
+    let shape          = CAShapeLayer()
+    let trackShape     = CAShapeLayer()
+    var circlePath     = UIBezierPath()
+    
+    var viewModel: TransitionViewModel
     var onDismiss: OnDismiss?
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+        self.startTimerAnimation()
+        self.ringTimerAnimation()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.2){
             self.dismiss(animated: true)
         }
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         onDismiss?()
+    }
+    
+    // MARK: - Ring Animation
+    
+    private func createCircleShapeLayer(strokeColor: UIColor, fillColor: UIColor) -> CAShapeLayer {
+        
+        let label = self.view.subviews.first { view in
+            return type(of: view) == UILabel.self
+        }
+                
+        let layer = CAShapeLayer()
+        let circularPath  = UIBezierPath(arcCenter: .zero, radius: 75, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        layer.path        = circularPath.cgPath
+        layer.strokeColor = self.circleAttributed.pulse.cgColor
+        layer.lineWidth   = 16
+        layer.fillColor   = fillColor.cgColor
+        layer.lineCap     = .round
+        layer.position    = label!.center
+        return layer
+    }
+    
+    func startTimerAnimation() {
+        
+        let label = self.view.subviews.first { view in
+            return type(of: view) == UILabel.self
+        }
+        
+        guard label != nil else { return }
+         
+        self.circlePath = UIBezierPath.init(arcCenter: label!.center, radius: 75, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        
+        pulsatingLayer = createCircleShapeLayer(strokeColor: .green, fillColor: UIColor.clear)
+        view.layer.addSublayer(pulsatingLayer)
+        
+        animatePulsatingLayer()
+                    
+        view.addShadow(layer: trackShape, path: self.circlePath, color: circleAttributed.shadowColor)
+        
+        trackShape.path         = circlePath.cgPath
+        trackShape.fillColor    = UIColor.clear.cgColor
+        trackShape.lineWidth    = 15
+        trackShape.strokeColor  = circleAttributed.trackColor.cgColor
+        view.layer.addSublayer(trackShape)
+
+        shape.path         = circlePath.cgPath
+        shape.lineWidth    = 15
+        shape.strokeColor  = circleAttributed.shapeColor.cgColor
+        shape.lineCap      = .round
+        shape.fillColor    = UIColor.clear.cgColor
+        shape.strokeEnd    = 0
+        view.layer.addSublayer(shape)
+    }
+    
+    private func animatePulsatingLayer() {
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        
+        animation.toValue        = 1.2
+        animation.duration       = 2.2
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        animation.autoreverses   = true
+        animation.repeatCount    = Float.infinity
+        
+        pulsatingLayer.add(animation, forKey: "pulsing")
+    }
+    
+    func ringTimerAnimation() {
+        
+        let startAnimate      = CABasicAnimation(keyPath: "strokeEnd")
+        
+        startAnimate.toValue  = 1
+        startAnimate.duration = CFTimeInterval(5)
+        startAnimate.fillMode = .forwards
+        startAnimate.isRemovedOnCompletion = false
+     
+        shape.add(startAnimate, forKey: "animation")
+    }
+    
+    func pauseRingAnimation() {
+        let pausedTime   = shape.convertTime(CACurrentMediaTime(), from: nil)
+        shape.speed      = 0
+        shape.timeOffset = pausedTime
+    }
+    
+    func resumeRingAnimation() {
+        let pausedTime = shape.timeOffset
+        
+        shape.speed      = 1
+        shape.timeOffset = 0
+        shape.beginTime  = 0
+        
+        let timeSincePause = shape.convertTime(CACurrentMediaTime(), from: nil) - pausedTime
+        shape.beginTime    = timeSincePause
     }
 }
