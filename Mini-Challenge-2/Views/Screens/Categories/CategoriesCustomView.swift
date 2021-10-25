e7662aaeca9df018adfee985556525fdaa36c80a
Johnny Camacho <joh_camacho@outlook.com>
2021-10-25 20:20:43 -0300

    Fix: Adding categories .xib

---
+++ Mini-Challenge-2/Views/Screens/Categories/CategoriesCustomView.swift
@@ -0,0 +1,67 @@
+//
+//  CategoriesCustomView.swift
+//  Mini-Challenge-2
+//
+//  Created by Johnny Camacho on 25/10/21.
+//
+
+import UIKit
+
+@IBDesignable
+class CategoriesCustomView: UIView {
+    
+    @IBInspectable var title: String = "Strength" {
+        didSet {
+            titleLabel.text = title
+        }
+    }
+    
+    @IBInspectable var quickDescription: String = "Quick Description" {
+        didSet {
+            descriptionLabel.text = quickDescription
+        }
+    }
+    
+    @IBInspectable var background: UIImage? {
+        didSet {
+            backgroundImageView.image = background
+        }
+    }
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    let nibName = "CategoriesCustomView"
+    var contentView: UIView?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        commonInit()
+    }
+    
+    func commonInit() {
+        guard let view = loadViewFromNib() else { return }
+        
+        view.frame = self.bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        self.addSubview(view)
+        
+        contentView = view
+    }
+    
+    func loadViewFromNib() -> UIView? {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+}
